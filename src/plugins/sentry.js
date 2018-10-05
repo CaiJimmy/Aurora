dae94063df91cf779a1fcccfa4f456467a4917a8
Jimmy Cai <jimmehcai@gmail.com>
2018-10-06 00:56:09 +0200

    fix(plugin/sentry): enable sentry only in production env.

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 
 const VUE_APP_SENTRY_API = process.env.VUE_APP_SENTRY_API;
 
-if (VUE_APP_SENTRY_API) {
+if (VUE_APP_SENTRY_API && process.env.NODE_ENV == 'production') {
     Sentry.init({
         dsn: VUE_APP_SENTRY_API,
         integrations: [new Sentry.Integrations.Vue({
