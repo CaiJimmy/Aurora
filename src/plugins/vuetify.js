a41a7b4aa605f6d1f91cfb7ddc6ed20677715e5f
Jimmy Cai <jimmehcai@gmail.com>
2019-06-08 18:50:54 +0200

    feat(Vuetify): add MDI font icon

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import store from '../store';
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 const opts = {
-    theme: store.getters['config/merged'].theme
+    theme: store.getters['config/merged'].theme,
+    icons: {
+        iconfont: 'mdi'
+    }
 };
 
 Vue.use(Vuetify);
