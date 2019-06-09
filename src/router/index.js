9c31e6d6be39461f730c459dbf03bc48030d87f2
Jimmy Cai <jimmehcai@gmail.com>
2019-06-09 20:08:43 +0200

    style(route): indent

--- src/router/index.js
+++ src/router/index.js
@@ -17,54 +17,56 @@ function loadView (view) {
 
 var router = new Router({
     mode: 'history',
-    routes: [{
-        path: '*',
-        component: loadView('NotFound')
-    },
-    {
-        path: '/restricted',
-        component: loadView('Restricted')
-    },
-    {
-        path: '/',
-        component: loadView('Home/App'),
-        auth: true
-    },
-    {
-        path: '/login',
-        component: loadView('Login/App'),
-    },
-    {
-        path: '/user/:userEmail',
-        component: loadView('Profile/App'),
-        props: true
-    },
-    {
-        path: '/a/',
-        component: loadView('Admin/App'),
-        meta: {
-            isAdmin: true
-        },
-        children: [{
-            path: '',
-            component: loadView('Admin/Home/App')
+    routes: [
+        {
+            path: '*',
+            component: loadView('NotFound')
         },
         {
-            path: 'settings',
-            component: loadView('Admin/Settings/App'),
+            path: '/restricted',
+            component: loadView('Restricted')
+        },
+        {
+            path: '/',
+            component: loadView('Home/App'),
+            auth: true
+        },
+        {
+            path: '/login',
+            component: loadView('Login/App'),
+        },
+        {
+            path: '/user/:userEmail',
+            component: loadView('Profile/App'),
+            props: true
+        },
+        {
+            path: '/a/',
+            component: loadView('Admin/App'),
+            meta: {
+                isAdmin: true
+            },
+            children: [
+                {
+                    path: '',
+                    component: loadView('Admin/Home/App')
+                },
+                {
+                    path: 'settings',
+                    component: loadView('Admin/Settings/App'),
 
-        },
-        {
-            path: 'taxonomy',
-            component: loadView('Admin/Taxonomy/App')
-        },
-        {
-            path: 'user',
-            component: loadView('Admin/User/App'),
+                },
+                {
+                    path: 'taxonomy',
+                    component: loadView('Admin/Taxonomy/App')
+                },
+                {
+                    path: 'user',
+                    component: loadView('Admin/User/App'),
 
+                }
+            ]
         }
-        ]
-    }
     ]
 });
 
