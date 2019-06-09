bd49e226f3b9086c70f04fdecc123c51678c116d
Jimmy Cai <jimmehcai@gmail.com>
2019-06-09 17:12:11 +0200

    feat: replace rgbaster with node-vibrant

--- src/utils/getColorFromImage/index.js
+++ src/utils/getColorFromImage/index.js
@@ -1,6 +1,16 @@
-import analyze from 'rgbaster'
+import * as Vibrant from 'node-vibrant'
 
-export default async function getColorFromImage(imageURL){
-    const result = await analyze(imageURL);
-    return result[0].color;
+export default function getColorFromImage (imageURL) {
+    return Vibrant.from(imageURL).getPalette()
+        .then((palette) => {
+            window.test = palette;
+            return {
+                DarkMuted: palette.DarkMuted.getHex(),
+                DarkVibrant: palette.DarkVibrant.getHex(),
+                LightMuted: palette.LightMuted.getHex(),
+                LightVibrant: palette.LightVibrant.getHex(),
+                Muted: palette.Muted.getHex(),
+                Vibrant: palette.Vibrant.getHex()
+            };
+        });
 }
