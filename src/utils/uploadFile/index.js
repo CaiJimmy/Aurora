3a23898f6859f05a95c425f89ede515a21ab5792
Jimmy Cai <jimmehcai@gmail.com>
2019-06-09 19:54:43 +0200

    fix(utils/uploadFile): remove console.log

--- src/utils/uploadFile/index.js
+++ src/utils/uploadFile/index.js
@@ -3,7 +3,6 @@ import {
 } from '@/firebase/storage';
 
 export default function uploadFile (file, path) {
-    console.log(file);
     let storageRef = Storage.ref(path || ("/" + file.name)),
         metadata = {
             contentType: file.type
