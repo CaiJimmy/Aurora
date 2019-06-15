4e5472a4fd901effb80526fd54f915d7231a6006
Jimmy Cai <jimmehcai@gmail.com>
2019-06-15 15:08:48 +0200

    fix(Login): non-whitelisted domain not logged out correctly

--- src/views/Login/validAccountCheck.js
+++ src/views/Login/validAccountCheck.js
@@ -6,38 +6,41 @@ import {
     Auth
 } from '@/firebase/auth';
 
-import Console from '@/utils/Console';
 import store from '@/store/';
 
 /**
  * Check if the account used for login is valid
  * 
- * @returns {Promise} Boolean which indicates account is valid or not
+ * @returns {Boolean} Boolean which indicates account is valid or not
  */
 async function validAccountCheck () {
     const user = Auth.currentUser,
         userRef = Firestore.collection('users').doc(user.email);
 
-    let userDoc = await userRef.get(),
+    let validUser = true;
+
+    let userDoc = await userRef.get().catch(() => validUser = false),
         newUserDoc = {
             'displayName': user.displayName.toLocaleUpperCase(),
             'photoURL': user.photoURL,
             'lastLogin': firestore.FieldValue.serverTimestamp()
         };
 
+    if (!validUser) {
+        // Not a white-listed domain
+        return validUser;
+    }
+
     if (!userDoc.exists || !userDoc.data().hasOwnProperty('role')) {
         /// If user does not exists, or it's profile doc doesn't have `role` defined, set it to config.login.defaultRole`
         newUserDoc.role = store.getters['config/merged'].login.defaultRole;
     }
 
-    return userRef.set(newUserDoc, {
+    await userRef.set(newUserDoc, {
         merge: true
-    }).then(() => {
-        return true;
-    }).catch((err) => { // Not a while-listed domain
-        Console.error(err);
-        return false;
     });
+
+    return validUser;
 }
 
 export default validAccountCheck;
